323801c770a1cfd9f4c57963c4a8d4db30821fd3
uchihiro <tohchi4122@gmail.com>
2023-01-20 15:51:36 +0900

    メソッド名の変更
    レビューを受け、メソッド名をcorrect_userメソッドからset_blogメソッドに変更しました。

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,7 @@
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  before_action :correct_user, only: %i[edit update destroy]
+  before_action :set_blog, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -51,7 +51,7 @@ class BlogsController < ApplicationController
 
   private
 
-  def correct_user
+  def set_blog
     @blog = current_user.blogs.find(params[:id])
   end
 
